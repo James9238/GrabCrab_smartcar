ad29755cc873e0beba2c7edb079ad8f1712ed6b2
James9238 <72325584+James9238@users.noreply.github.com>
2021-01-06 14:26:29 +0100

    Description

--- DC_Control/DC_Control.ino
+++ DC_Control/DC_Control.ino
@@ -1,4 +1,22 @@
-/*Joystick DC Control for GrabCrab smartcar*/
+// GrabCrab Smartcar project //
+
+/*
+Medlemmer: Henrik
+Audun, Linda (når de får ut fingern) :)
+
+Mål:
+Smart-kjøretøy med mangfoldig eller spesifikke muligheter. Fyll inn mer.
+*/
+
+// TODO //
+/*
+Logic for minimal inputs when using RPi for motor and servo control. 
+Servo control: Serial comms? Degree or step input?
+Motor control: Logic circuit? f.ex if ((forward OR reverse) AND strafe) -> slope to strafe direction. 
+*/
+
+
+// Define inputs, outputs and global variables:
 
 /*Nano PWM Pinout: 3, 5, 6, 9, 10, 11 // 490Hz(980Hz: pin 5, 6*/
 /*Mega PWM Pinout: 2-13, 44-46*/
@@ -34,13 +52,13 @@
 
 #define potPin          A0
 
-
 /*Motor speed variables*/
 int motorSpeedLF = 0;
 int motorSpeedLB = 0;
 int motorSpeedRF = 0;
 int motorSpeedRB = 0;
 
+/*Variable for interrupt routine*/
 bool EMERGENCY_STOP = false;
 #define STOP_LED A5
 
