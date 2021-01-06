1dcf57a4c1dc5c26d268a4ef51f4033c5e74f205
James9238 <72325584+James9238@users.noreply.github.com>
2021-01-06 14:28:57 +0100

    Description

--- DC_Control/DC_Control.ino
+++ DC_Control/DC_Control.ino
@@ -11,8 +11,12 @@ Smart-kjøretøy med mangfoldig eller spesifikke muligheter. Fyll inn mer.
 // TODO //
 /*
 Logic for minimal inputs when using RPi for motor and servo control. 
-Servo control: Serial comms? Degree or step input?
-Motor control: Logic circuit? f.ex if ((forward OR reverse) AND strafe) -> slope to strafe direction. 
+  Servo control: Serial comms? Degree or step input?
+  Motor control: Logic circuit? f.ex if ((forward OR reverse) AND strafe) -> slope to strafe direction. 
+                 Keyword serial control?
+Is it possible to run both motors and servo from the same nano?
+  Figure out pinout requirements with everything connected. 
+  Alternate control solutions if too few?
 */
 
 
